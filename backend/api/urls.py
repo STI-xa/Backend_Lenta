036a2209a812d5da31038c6a174005a99cc6ed27
Mikhail Moskovkin <anstane228@gmail.com>
2023-09-24 23:07:56 +0300

    First commit

---
+++ backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+
+from rest_framework import routers
+
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
