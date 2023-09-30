900f2a2bb403d3853c3c3d86c0aad79a3ffba33a
Полина Николаева <sti-xa@ya.ru>
2023-09-30 23:43:17 +0300

    added logger

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 
+from backend.logger import log_exceptions, logger_factory
 from sales.models import SKU, Shop, Sales, Forecast
 from .serializers import (
     SKUSerializer,
@@ -14,21 +15,28 @@ from .serializers import (
 )
 
 
+logger = logger_factory(__name__)
+
+
+@log_exceptions(logger)
 class SKUViewSet(viewsets.ModelViewSet):
     queryset = SKU.objects.all()
     serializer_class = SKUSerializer
 
 
+@log_exceptions(logger)
 class ForecastViewSet(viewsets.ModelViewSet):
     queryset = Forecast.objects.all()
     serializer_class = ForecastSerializer
 
 
+@log_exceptions(logger)
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = SKU.objects.all()
     serializer_class = CategorySerializer
 
 
+@log_exceptions(logger)
 class SalesViewSet(viewsets.ViewSet):
     def list(self, request):
         sales_data = Sales.objects.all()
@@ -36,11 +44,13 @@ class SalesViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
 
+@log_exceptions(logger)
 class ShopViewSet(viewsets.ModelViewSet):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
 
 
+@log_exceptions(logger)
 class ForecastInputViewSet(viewsets.ViewSet):
     def create(self, request):
         serializer = ForecastInputSerializer(data=request.data)
@@ -53,6 +63,7 @@ class ForecastInputViewSet(viewsets.ViewSet):
                 serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@log_exceptions(logger)
 class ForecastOutputViewSet(viewsets.ViewSet):
     def list(self, request):
         forecast_data = Forecast.objects.all()
