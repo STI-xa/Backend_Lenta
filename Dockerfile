a3562e07dce20b0c215a1c71832b56f4aa843708
Полина Николаева <sti-xa@ya.ru>
2023-10-06 00:50:20 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,14 @@
 FROM python:3.11.4-slim
 
 
-ENV WORKDIR /app/backend
-
-WORKDIR $WORKDIR
+WORKDIR /app
 
 COPY requirements.txt .
 
 RUN python -m pip install --upgrade pip
 RUN pip3 install -r requirements.txt --no-cache-dir
 
-COPY . .
-
-EXPOSE 8000
+COPY backend/ /app
 
 
-CMD ["gunicorn", "foodgram_project.wsgi:application", "--bind", "0:8000" ]
+CMD ["gunicorn", "backend.wsgi:application", "--bind", "0:8000" ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,24 +68,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE':'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME', 'postgres'),
-#         'USER': os.getenv('POSTGRES_USER', 'postgres_user'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-#         'HOST': os.getenv('POSTGRES_HOST', ''),
-#         'PORT': os.getenv('POSTGRES_PORT', 5432)
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', default='postgres'),
+        'PORT': os.getenv('POSTGRES_PORT', default='5432')
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
@@ -123,6 +123,10 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
